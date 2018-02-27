d72b97b8ec374ef881a6b3e457171801747fa67d
Zoe Ames <zoeames@gmail.com>
2018-02-26 20:41:28 -0600

    finished challenge 3

--- strings.js
+++ strings.js
@@ -6,3 +6,9 @@ console.log("numOfSandwhiches", numOfSandwhiches);
 var name = "zoe";
 var string = "Hello, " + name + " how are you doing today?";
 console.log("string:", string);
+
+// Challenge #3
+var DNA = "GCAT";
+// var RNA = DNA.replace('T', 'U');
+var RNA = DNA.replace(/T/g, 'U');
+console.log("RNA", RNA);

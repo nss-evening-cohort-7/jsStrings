43235d39a9edc8ce6abcbf3966160329807c6e93
Zoe Ames <zoeames@gmail.com>
2018-02-26 20:30:46 -0600

    added challenge 2

--- strings.js
+++ strings.js
@@ -1,3 +1,8 @@
 // Challenge #1
 var numOfSandwhiches = 4*2;
 console.log("numOfSandwhiches", numOfSandwhiches);
+
+// Challenge #2
+var name = "zoe";
+var string = "Hello, " + name + " how are you doing today?";
+console.log("string:", string);
